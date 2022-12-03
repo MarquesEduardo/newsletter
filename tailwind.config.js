5e4584ca634ed0e916fc5b41ac7fd9db4ab80733
Eduardo Marques <eduardomarques00@hotmail.com>
2022-12-03 01:12:04 -0300

    feat: update base files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/index.html"],
+  content: ['./index.html'],
   mode: 'jit',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        azul: {
+          claro: '#C5DFFF',
+          escuro: '#061E3C',
+          hover: '#1057B0',
+        },
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
